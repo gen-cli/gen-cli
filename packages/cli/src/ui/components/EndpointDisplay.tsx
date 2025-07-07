04baa0b411df5ac9e992dc15de336de25b95d46f
Shenghang Tsai <jackalcooper@gmail.com>
2025-07-08 03:13:13 +0800

    Config and display of baseURL

---
+++ packages/cli/src/ui/components/EndpointDisplay.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box, Text } from 'ink';
+
+interface EndpointDisplayProps {
+  baseURL?: string;
+}
+
+export const EndpointDisplay: React.FC<EndpointDisplayProps> = ({
+  baseURL,
+}) => (
+    <Box>
+      <Text>
+        {baseURL}
+      </Text>
+    </Box>
+  );
