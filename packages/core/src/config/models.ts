940dc80b6421204d871b0ef1240a2c6f99c478d4
Shenghang Tsai <jackalcooper@gmail.com>
2025-07-17 11:28:52 +0800

    try fix

--- packages/core/src/config/models.ts
+++ packages/core/src/config/models.ts
@@ -4,13 +4,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const DEFAULT_MODEL = process.env.CI
+export function isSiliconFlow(): boolean {
+  return !!(!process.env.CI || process.env.SILICONFLOW_API_KEY);
+}
+
+export const DEFAULT_MODEL = isSiliconFlow()
   ? 'gemini-2.5-pro'
   : 'deepseek-ai/DeepSeek-V3';
-export const DEFAULT_FLASH_MODEL = process.env.CI
+export const DEFAULT_FLASH_MODEL = isSiliconFlow()
   ? 'gemini-2.5-flash'
   : 'deepseek-ai/DeepSeek-V3';
-export const DEFAULT_EMBEDDING_MODEL = process.env.CI
+export const DEFAULT_EMBEDDING_MODEL = isSiliconFlow()
   ? 'gemini-embedding-001'
   : 'Qwen/Qwen3-Embedding-8B';
 
